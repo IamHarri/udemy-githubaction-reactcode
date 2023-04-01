0d4dd087935ed09b7ade692f2dab02584047b687
Long Ly <long18161245@gmail.com>
2023-04-01 15:29:46 +0700

    feat: create release notify

--- release.config.js
+++ release.config.js
@@ -1,16 +1,17 @@
 module.exports = {
-    branches: ["main"],
-    repositoryUrl: "https://github.com/longlc3/udemy-githubaction-reactcode",
-    tagFormat: "v${version}",
-    plugins: [
-        '@semantic-release/commit-analyzer', 
-        '@semantic-release/release-notes-generator', 
-        ['@semantic-release/github', {
-            successComment: false,
-            failTitle: false,
-            "assets": [
-                { path: "package.json", label: "package"}
-            ]
-        }]  
-    ]
-}
+  branches: ["main"],
+  repositoryUrl: "https://github.com/longlc3/udemy-githubaction-reactcode",
+  tagFormat: "v${version}",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/github",
+      {
+        successComment: false,
+        failTitle: false,
+        assets: [{ path: "package.json", label: "package" }],
+      },
+    ],
+  ],
+};
