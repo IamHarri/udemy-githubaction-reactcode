dee5705d0ddb6d703eae7be34460df2a71622192
longlc3 <94947386+longlc3@users.noreply.github.com>
2023-04-01 12:09:24 +0700

    Feature/config cicd (#3)
    * initial
    
    * add github action
    
    * change node version
    
    * add check format
    
    * change code owner
    
    * add staging
    
    * format code
    
    * add cache
    
    * add format step
    
    * add push artifact
    
    * update condition
    
    * fix: fix decription
    
    * fix: fix decription

---
+++ release.config.js
@@ -0,0 +1,10 @@
+mpdule.exports = {
+    branches: ["main"],
+    repositoryUrl: "https://github.com/longlc3/udemy-githubaction-reactcode",
+    tagFormat: "v${version}",
+    plugins: [
+        '@semantic-release/commit-analyzer', 
+        '@semantic-release/release-notes-generator', 
+        '@semantic-release/github'
+    ]
+}
